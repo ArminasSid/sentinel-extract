636487e04a1daec2a7430477a1a6f93df0292cce
ArminasSid <armis920@gmail.com>
2022-12-29 02:21:24 +0200

    TMP progress of refactoring warper functions

--- src/warper.py
+++ src/warper.py
@@ -5,7 +5,67 @@ import shutil
 from tqdm import tqdm
 import numpy as np
 import warnings
+from glob import glob
+from dataclasses import dataclass
+
 warnings.filterwarnings("ignore")
+
+
+@dataclass
+class Coordinates:
+    xmin: float
+    ymax: float
+    xmax: float
+    ymin: float
+
+
+@dataclass
+class Product:
+    custom_raster_path: str
+    tci_raster_path: str
+    mask_raster_path: str
+
+    custom_raster: gdal.Dataset = None
+    tci_raster: gdal.Dataset = None
+    mask_raster: gdal.Dataset = None
+
+    coordinates: Coordinates = None
+
+    def __post_init__(self):
+        self.custom_raster = gdal.Open(self.custom_raster_path)
+        self.tci_raster = gdal.Open(self.tci_raster_path)
+        self.mask_raster = gdal.Open(self.mask_raster_path)
+
+        self.coordinates = Coordinates(
+            self.get_raster_coordinates(raster=self.tci_raster))
+
+    @staticmethod
+    def get_raster_coordinates(raster):
+        """
+        Get raster coordinates from gdal.Dataset object
+        """
+        gt = raster.GetGeoTransform()
+        xmin = gt[0]
+        ymax = gt[3]
+        res = gt[1]
+        xlen = res * raster.RasterXSize
+        ylen = res * raster.RasterYSize
+        xmax = xmin + xlen
+        ymin = ymax - ylen
+        return [xmin, ymax, xmax, ymin]
+
+    def fits_coord_req(self, w_x_y) -> bool:
+        """
+        Checks whether product is a super set of wanted x and y coordinates (w_x_y)
+
+        w_x_y - Wanted coordinate bounds
+        """
+        if (self.coordinates.xmin < w_x_y[0]
+                and self.coordinates.ymax > w_x_y[1]
+                and self.coordinates.xmax > w_x_y[2]
+                and self.coordinates.ymin < w_x_y[3]):
+            return True
+        return False
 
 
 def parse_args():
@@ -52,18 +112,20 @@ def process_output_folder(output_path: str, overwrite_allowed: bool):
 
 def tile_coords(tile):
     if str(tile).lower() == 'south':
-        ul_x_y = [22.620849609375, 54.983918190363234]
-        lr_x_y = [26.015625, 53.87844040332883]
+        return Coordinates(
+            xmin=22.620849609375, ymax=54.983918190363234,
+            xmax=26.015625000000, ymin=53.878440403328830
+        )
     elif str(tile).lower() == 'north':
-        ul_x_y = [20.8411111, 56.46855975598276]
-        lr_x_y = [26.87255859375, 54.895564790773385]
+        return Coordinates(
+            xmin=20.84111110000, ymax=56.46855975598276,
+            xmax=26.87255859375, ymin=54.89556479077338
+        )
     else:
         raise KeyError('Tile name not found.')
-    return ul_x_y, lr_x_y
 
 
 def image_coordinates(img):
-    #img = gdal.Open(image)
     gt = img.GetGeoTransform()
     xmin = gt[0]
     ymax = gt[3]
@@ -111,6 +173,9 @@ def calculate_iterations(starting_point, end_point, step):
     else:
         return ((end_point - starting_point) / step)
 
+def calculate_iterations1(bounds, step):
+    
+
 
 def wanted_x_y(ul_x_y, step, i_offset, j_offset):
     return [
@@ -132,6 +197,24 @@ def filter_images(images, masks, coords, w_x_y):
             filt_msk.append(mask)
             filt_coords.append(coord)
     return filt_img, filt_msk, filt_coords
+
+
+def filter_images1(custom_rasters, tci_rasters, mask_rasters, coords_rasters, w_x_y):
+    """
+    Filter rasters by wanted coordinates (w_x_y).
+    """
+    custom_rasters_fltr, tci_rasters_fltr = []
+    mask_rasters_fltr, coords_rasters_fltr = []
+
+    # Filter rasters that fit needed coords
+    zipped = zip(custom_rasters, tci_rasters, mask_rasters, coords_rasters)
+    for custom, tci, mask, coord in zipped:
+        if check_if_fits_req(coord, w_x_y):
+            custom_rasters_fltr.append(custom)
+            tci_rasters_fltr.append(tci)
+            mask_rasters_fltr.append(mask)
+            coords_rasters_fltr.append(coord)
+    return custom_rasters_fltr, tci_rasters_fltr, mask_rasters_fltr, coords_rasters_fltr
 
 
 def warp_memory(image, bounds):
@@ -195,6 +278,32 @@ def cut_normal(tile, ul_x_y, lr_x_y, _input_images, _input_masks, output_folder,
                 bounds, output_image, filtered_images, filtered_masks, rgb)
 
 
+def cut_normal1(tile, warping_bounds, products, output_folder, step):
+    i_range = range(int(calculate_iterations(ul_x_y[0], lr_x_y[0], step)))
+    j_range = range(int(calculate_iterations(ul_x_y[1], lr_x_y[1], step)))
+    _input_coords = []
+    for image in _input_images:
+        _input_coords.append(image_coordinates(image))
+    tbar = tqdm(i_range)
+    for i in tbar:
+        for j in j_range:
+            w_x_y = wanted_x_y(ul_x_y, step, i, j)
+
+            filtered_images, filtered_masks, filtered_coords = filter_images(
+                _input_images,
+                _input_masks,
+                _input_coords,
+                w_x_y
+            )
+            number = '{0}_{1}'.format(str(i).zfill(5), str(j).zfill(5))
+            output_image = "{}/image_{}_{}.jp2".format(
+                output_folder, tile, number)
+            bounds = [w_x_y[0], w_x_y[3], w_x_y[2], w_x_y[1]]
+
+            produce_image_without_clouds(
+                bounds, output_image, filtered_images, filtered_masks, rgb)
+
+
 def load_rasters(image_paths):
     rasters = []
     for path in image_paths:
@@ -210,26 +319,43 @@ def is_image_rgb(image):
         return False
 
 
+def get_rasters(input_folder: str):
+    fci_rasters = glob(pathname=f'{input_folder}/*FCI*.tiff')
+    tci_rasters = glob(pathname=f'{input_folder}/*TCI*.tiff')
+    mask_rasters = glob(pathname=f'{input_folder}/*mask*.tiff')
+
+    return fci_rasters, tci_rasters, mask_rasters
+
+
 def warp(input_folder, output_folder):
     files = os.listdir(input_folder)
-    input_images = []
-    input_masks = []
-    for i in range(int(len(files)/2)):
-        input_images.append('{}/image_{}.jp2'.format(input_folder, i))
-        input_masks.append('{}/mask_{}.jp2'.format(input_folder, i))
-    print("Detected {} images, loading images as rasters...".format(len(input_images)))
 
-    images = load_rasters(input_images)
-    masks = load_rasters(input_masks)
+    fci_rasters, tci_rasters, mask_rasters = get_rasters(
+        input_folder=input_folder)
+
+    products = []
+    # Assemble products into a list of Product objects
+    for fci_raster, tci_raster, mask_raster in zip(fci_rasters, tci_rasters, mask_rasters):
+        products.append(Product(
+            custom_raster_path=fci_raster,
+            tci_raster_path=tci_raster,
+            mask_raster_path=mask_raster
+        ))
+
+    print("Detected {} images, loading images as rasters...".format(len(products)))
+
     tiles = ["north", "south"]
-    print("Images and masks have been loaded in. Proceding to warping.")
 
-    rgb = is_image_rgb(images[0])
+    rgb = is_image_rgb(tci_rasters[0])
 
     step = 0.00642
     for tile in tiles:
         print("Forming {} part".format(tile))
-        ul_x_y, lr_x_y = tile_coords(tile)
+        bounds = tile_coords(tile=tile)
+        cut_normal(
+            tile=tile,
+
+        )
         cut_normal(tile, ul_x_y, lr_x_y, images,
                    masks, output_folder, step, rgb)
 
